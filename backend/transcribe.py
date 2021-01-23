7653cdc6801e944ac923e4ce4cf17f55ae2f2862
mlunghi <matteo.lunghi2000@gmail.com>
2021-01-23 21:59:00 +0100

    channel name and video title

--- backend/transcribe.py
+++ backend/transcribe.py
@@ -31,9 +31,15 @@ class TranscribeAPI(Resource):
         url = data['youtubeURL']
         note = data['note']
 
+        #pulls video title and channel name
+        resp = requests.get("https://noembed.com/embed?url=https://www.youtube.com/watch?v=tn2AgrwXpNQ")
+        resp = resp.json()
+        channel_name = resp["author_name"]
+        video_title = resp["title"]
+
         #finds video id
         video_id = self.parse_url(url)
         transcript = YouTubeTranscriptApi.get_transcript(video_id)
-        print(transcript)
+        
 
-        return { "response" : "200:SUCCESS"}
+        return {"response" : "200:SUCCESS"}
