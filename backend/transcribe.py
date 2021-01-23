366df3af311e23f64a1c7e684f3ea946ea04a1f0
mlunghi <matteo.lunghi2000@gmail.com>
2021-01-23 20:06:46 +0100

    backed v1 finished

--- backend/transcribe.py
+++ backend/transcribe.py
@@ -9,23 +9,31 @@ import time
 import requests
 from flask import request
 from flask_restful import Resource, reqparse
+from youtube_transcript_api import YouTubeTranscriptApi
 
 
 class TranscribeAPI(Resource):
 
     #parses youtube url to find video id
     def parse_url(self, url):
-        return url[url.find("v")+2 : url.find("&")]
+        if "&" in url:
+            id = url[url.find("v")+2 : url.find("&")+1]
+        else:
+            id = url[url.find("v")+2 : ]
+        return id
 
-
+    #handles post request
     def post(self):
 
         #pulls data from api request
         data = request.get_json()
         user_id = data['userID']
         url = data['youtubeURL']
-        note = data['user']
+        note = data['note']
 
+        #finds video id
         video_id = self.parse_url(url)
+        transcript = YouTubeTranscriptApi.get_transcript(video_id)
+        print(transcript)
 
         return { "response" : "200:SUCCESS"}
---
+++ run.py
@@ -0,0 +1,20 @@
+from flask import Flask, send_from_directory
+from backend.transcribe import TranscribeAPI
+
+app = Flask(__name__)
+
+
+def create_app(config_filename):
+    #app.config.from_object(config_filename)
+
+    from app import api_bp
+    app.register_blueprint(api_bp, url_prefix='/api')
+
+    return app
+
+
+
+
+if __name__ == "__main__":
+    app = create_app("config")
+    app.run(debug=True)
